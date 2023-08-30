401babe1188f1c57729d0e50e27aeec2db8fa0ac
JoseLuisMJ <trepa01@hotmail.com>
2023-08-30 09:42:18 +0200

    routing

--- frontend/laligaf5/src/app/app-routing.module.ts
+++ frontend/laligaf5/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'events', pathMatch: 'full' },
+  { path: 'events', loadChildren: () => import('./events/events.module').then(m => m.EventsModule)}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/laligaf5/src/app/events/events-routing.module.ts
+++ frontend/laligaf5/src/app/events/events-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StaffComponent } from './views/staff/staff.component';
+import { HomeComponent } from './views/home/home.component';
+import { TeamsComponent } from './views/teams/teams.component';
+import { TeamsdetailsComponent } from './views/teamsdetails/teamsdetails.component';
+import { CalendarComponent } from './views/calendar/calendar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent},
+  { path: 'teams', component: TeamsComponent},
+  { path: 'teamsdetails', component: TeamsdetailsComponent},
+  { path: 'staff', component: StaffComponent},
+  { path: 'calendar', component: CalendarComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
